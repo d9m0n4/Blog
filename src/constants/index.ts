1e21188cbc1f408fa8a02454a270f398b439bc44
d9m0n4 <c2h8e9d4@gmail.com>
2022-09-26 15:39:50 +0300

    build

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASEURL = 'http://localhost:5000/';
+export const BASEURL = 'https://still-harbor-47481.herokuapp.com/';
 export const MAIN_TAGS_CATEGORY = 'Все';
 export const PAGE_LIMIT = 5;
 export const DEFAULT_PAGE = 1;
