8776149ef3b2221dfafbafa1e104361bde99271f
d9m0n4 <c2h8e9d4@gmail.com>
2024-02-17 14:10:05 +0300

    server

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASEURL = 'https://blog-backend-production-3f90.up.railway.app/';
+export const BASEURL = 'http://blog-backend.na4u.ru/';
 export const MAIN_TAGS_CATEGORY = 'Все';
 export const PAGE_LIMIT = 5;
 export const DEFAULT_PAGE = 1;
