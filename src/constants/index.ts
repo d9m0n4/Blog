05ce81ca3f822d12602e835b92e950e2838d8f95
d9m0n4 <c2h8e9d4@gmail.com>
2024-02-21 14:00:33 +0300

    backend api

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const BASEURL = 'http://blog-backend.na4u.ru/';
+export const BASEURL = 'https://blog-43.na4u.ru/';
 export const MAIN_TAGS_CATEGORY = 'Все';
 export const PAGE_LIMIT = 5;
 export const DEFAULT_PAGE = 1;
